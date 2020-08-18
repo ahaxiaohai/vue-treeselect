8926f3182092408dd8e272b92a8f8fceb7a99bf7
zhanghaiyong <zhanghaiyong@qq.com>
2020-08-18 17:51:45 +0800

    xiug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Treeselect from './components/Treeselect'
 import treeselectMixin from './mixins/treeselectMixin'
+import renderInput from './components/Input'
 import './style.less'
 
 export default Treeselect
-export { Treeselect, treeselectMixin }
+export { Treeselect, treeselectMixin,renderInput }
 export {
   // Delayed loading.
   LOAD_ROOT_OPTIONS,
